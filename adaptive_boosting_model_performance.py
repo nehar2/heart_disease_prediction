7731ea5739436c1d2f128653f6bc5b53a6772d06
nehar2 <neharachapudi3d@gmail.com>
2020-08-19 12:43:29 -0700

    testing_set_performance

---
+++ adaptive_boosting_model_performance.py
@@ -0,0 +1,30 @@
+
+import pandas as pd
+import pickle
+from sklearn.metrics import f1_score
+
+x_test = pd.read_csv('output_data/heart_disease_data_x_test.csv')
+x_test_replaced = pd.read_csv('output_data/heart_disease_data_x_test_replaced.csv')
+y_test = pd.read_csv('output_data/heart_disease_data_y_test.csv')
+
+x_test_replaced = x_test_replaced.set_index('patientid')
+x_test_replaced = x_test_replaced.reindex(index=x_test['patientid'])
+x_test_replaced = x_test_replaced.reset_index()
+
+y_test = y_test.set_index('patientid')
+y_test = y_test.reindex(index=x_test['patientid'])
+y_test = y_test.reset_index()
+
+clf = pickle.load(open('pickle_files/adaptive_boosting_model.pickle', 'rb'))
+y_pred = clf.predict(x_test_replaced)
+data = pd.DataFrame({'y_prediction': y_pred, 'y_actual': y_test['num'], 'location': x_test['origin']})
+info = {'location':[], 'f1_score':[], 'size':[]}
+
+for location in data['location'].unique():
+	data_location = data.loc[data['location'] == location]
+	location_f1_score = f1_score(data_location['y_prediction'], data_location['y_actual'], average='weighted')
+	info['location'].append(location)
+	info['f1_score'].append(location_f1_score)
+	info['size'].append(data_location.shape[0])
+
+pd.DataFrame(info).to_csv('charts/adaptive_boosting_model_performance.csv', index=None)
--- testing_set.py
+++ testing_set.py
@@ -1,52 +1,57 @@
 
 import pandas as pd
 import sklearn
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn import tree
+from sklearn import linear_model
+from sklearn.linear_model import LogisticRegression
 import xgboost
 from xgboost import XGBClassifier
 from sklearn.metrics import f1_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 import warnings
+import pickle
 
 warnings.filterwarnings('ignore')
 
-train_x = pd.read_csv('output_data/heart_disease_data_x_train_replaced.csv')
-train_y = pd.read_csv('output_data/heart_disease_data_y_train.csv')
-test_x = pd.read_csv('output_data/heart_disease_data_x_test_replaced.csv')
-test_y = pd.read_csv('output_data/heart_disease_data_y_test.csv')
+x_train = pd.read_csv('output_data/heart_disease_data_x_train_replaced.csv')
+y_train = pd.read_csv('output_data/heart_disease_data_y_train.csv')
+x_test = pd.read_csv('output_data/heart_disease_data_x_test_replaced.csv')
+y_test = pd.read_csv('output_data/heart_disease_data_y_test.csv')
 
 scores = {'algorithm':[], 'f1_score':[], 'precision_score':[], 'recall_score':[]}
 
-### RANDOM FOREST ### 
-clf = RandomForestClassifier(min_samples_split=6, n_estimators=94, criterion='gini', bootstrap=True)
-clf.fit(train_x, train_y['num'])
-y_pred = clf.predict(test_x)
-scores['algorithm'].append('random_forest')
-scores['f1_score'].append(f1_score(y_pred, test_y['num'], average='weighted'))
-scores['precision_score'].append(precision_score(y_pred, test_y['num'], average='weighted'))
-scores['recall_score'].append(recall_score(y_pred, test_y['num'], average='weighted'))
-
 ### ADAPTIVE BOOSTING ###
-clf = AdaBoostClassifier(n_estimators=17, learning_rate=0.09300000000000001, base_estimator=tree.DecisionTreeClassifier(max_depth=2))
-clf.fit(train_x, train_y['num'])
-y_pred = clf.predict(test_x)
+clf = AdaBoostClassifier(n_estimators=17, learning_rate=0.093, base_estimator=tree.DecisionTreeClassifier(max_depth=2))
+clf.fit(x_train, y_train['num'])
+pickle.dump(clf, open('pickle_files/adaptive_boosting_model.pickle', 'wb'))
+y_pred = clf.predict(x_test)
 scores['algorithm'].append('adaptive_boosting')
-scores['f1_score'].append(f1_score(y_pred, test_y['num'], average='weighted'))
-scores['precision_score'].append(precision_score(y_pred, test_y['num'], average='weighted'))
-scores['recall_score'].append(recall_score(y_pred, test_y['num'], average='weighted'))
+scores['f1_score'].append(f1_score(y_pred, y_test['num'], average='weighted'))
+scores['precision_score'].append(precision_score(y_pred, y_test['num'], average='weighted'))
+scores['recall_score'].append(recall_score(y_pred, y_test['num'], average='weighted'))
+
+### LOGISTIC REGRESSION ###
+clf = LogisticRegression(solver='newton-cg')
+clf.fit(x_train, y_train['num'])
+pickle.dump(clf, open('pickle_files/logistic_regression_model.pickle', 'wb'))
+y_pred = clf.predict(x_test)
+scores['algorithm'].append('logistic_regression')
+scores['f1_score'].append(f1_score(y_pred, y_test['num'], average='weighted'))
+scores['precision_score'].append(precision_score(y_pred, y_test['num'], average='weighted'))
+scores['recall_score'].append(recall_score(y_pred, y_test['num'], average='weighted'))
 
 ### XGBOOST ###
-clf = XGBClassifier(n_estimators=14, learning_rate=0.09300000000000001, max_depth=2)
-clf.fit(train_x, train_y['num'])
-y_pred = clf.predict(test_x)
+clf = XGBClassifier(n_estimators=14, learning_rate=0.093, max_depth=2)
+clf.fit(x_train, y_train['num'])
+pickle.dump(clf, open('pickle_files/xgboost_model.pickle', 'wb'))
+y_pred = clf.predict(x_test)
 scores['algorithm'].append('xgboost')
-scores['f1_score'].append(f1_score(y_pred, test_y['num'], average='weighted'))
-scores['precision_score'].append(precision_score(y_pred, test_y['num'], average='weighted'))
-scores['recall_score'].append(recall_score(y_pred, test_y['num'], average='weighted'))
+scores['f1_score'].append(f1_score(y_pred, y_test['num'], average='weighted'))
+scores['precision_score'].append(precision_score(y_pred, y_test['num'], average='weighted'))
+scores['recall_score'].append(recall_score(y_pred, y_test['num'], average='weighted'))
 
+### SAVING SCORES ###
 pd.DataFrame(scores).to_csv('charts/testing_set_scores.csv', index=None)
-
 
